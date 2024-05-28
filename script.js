2070c91b9e8d2cbf5f98dbf3f48254a6b15bd763
HolaMojo <holamojo.jojo@gmail.com>
2024-05-28 03:29:28 +0000

    attempt

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const abbreviations = [
     { abbreviation: "AOR", meaning: "Approval of Requirements", context: ["A document that requests approval from suitable approving authorities for a specific amount of funding governed by specific requirments."] },
     { abbreviation: "TOS", meaning: "Theory of Success", context: ["A framework that hypothesises the key factors that will make a programme or project successful (core ingredients) and determines the yardstick by which to measure success."] }, 
     { abbreviation: "AOP", meaning: "Approval of Programme", context: ["A document that is written after funding has been secured by an AOR paper that requests for funding for a specific programme that draws down from the funds detailed in the AOR."] }, 
+    { abbreviation: "DLAM", meaning: "Do Less Achieve More", context: ["A phrase meaning to examine a situation to see how to achieve maximum output while manageable. Sometimes used as a verb, eg: to Dee-Lamb something."] },
 ];
 
 // console.log(abbreviations)
